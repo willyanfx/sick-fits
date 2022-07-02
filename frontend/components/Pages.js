56c753614d26f202d90444719d7ddf9e49f47d7f
willyanfx <willyan.fx@gmail.com>
2022-07-01 19:18:14 -0700

    feat: nprogress router css animation

--- frontend/components/Pages.js
+++ frontend/components/Pages.js
@@ -1,12 +1,71 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    box-sizing: border-box;
+
+  }
+
+  *, *::before, *:after {
+    box-sizing: inherit;
+  }
+  
+  root {
+    --red: #ff0000;
+    --black: #3939339;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rbga(0,0,0,0.09);
+
+  }
+
+  body {
+    font-family: 'radnika_next',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+
+  button {
+    font-family: 'radnika_next',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      <h2>I am page component</h2>
-      <p>{children}</p>
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,11 @@
+import nProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Pages';
+import '../components/styles/nprogress.css'; // having error, will import late
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 export default function MyApp({ Component, pageProps }) {
   return (
