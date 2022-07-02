60f1cceb7f3ea5eb3cf5b8ffa3f05bf7ec61f484
willyanfx <willyan.fx@gmail.com>
2022-07-01 19:17:42 -0700

    feat: styles

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -6,6 +6,7 @@ export default function Nav() {
       <Link href="/products">Products</Link>
       <Link href="/sell">Sell</Link>
       <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
     </nav>
   );
 }
