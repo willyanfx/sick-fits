8d49f1158e04594ea1f65e8830f0d4e0fa2ee41e
willyanfx <willyan.fx@gmail.com>
2022-07-05 22:07:06 -0700

    feat: custom usehook form

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  const [input, setInput] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInput(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.entries(input).map(([key, value]) => [key, '']);
+    setInput(Object.fromEntries(blankState));
+  }
+
+  return { input, handleChange, resetForm, clearForm };
+}
