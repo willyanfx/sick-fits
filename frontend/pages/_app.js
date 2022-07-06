b59917e3b878bf005ba41a256f3839e4b0a141cb
willyanfx <willyan.fx@gmail.com>
2022-07-05 20:09:31 -0700

    feat: added apollo client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,15 +2,30 @@ import nProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Pages';
 import '../components/styles/nprogress.css'; // having error, will import late
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', nProgress.start);
 Router.events.on('routeChangeError', nProgress.done);
 Router.events.on('routeChangeComplete', nProgress.done);
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,1 @@
-import Page from '../components/Pages';
-
-export default function IndexPage() {
-  return (
-    <>
-      <p>Hello world</p>
-    </>
-  );
-}
+export { default } from './products';
