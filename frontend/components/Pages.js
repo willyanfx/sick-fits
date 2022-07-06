0cf80dbabeb8a0b6f5e8484183d8a67697a95311
willyanfx <willyan.fx@gmail.com>
2022-07-05 21:24:30 -0700

    fix: global styles

--- frontend/components/Pages.js
+++ frontend/components/Pages.js
@@ -9,49 +9,39 @@ const GlobalStyles = createGlobalStyle`
     font-weight: normal;
     font-style: normal;
   }
-
   html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
     font-size: 10px;
   }
-
-  *, *::before, *:after {
+  *, *:before, *:after {
     box-sizing: inherit;
   }
-  
-  html {
-    --red: #ff0000;
-    --black: #3939339;
-    --grey: #3a3a3a;
-    --gray: var(--grey);
-    --lightGrey: #e1e1e1;
-    --offWhite: #ededed;
-    --maxWidth: 1000px;
-    --bs: 0 12px 24px 0 rbga(0,0,0,0.09);
-
-  }
-
   body {
-    font-family: 'radnika_next',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     padding: 0;
     margin: 0;
     font-size: 1.5rem;
-    line-height: 2;
+    line-height:2;
   }
-
   a {
     text-decoration: none;
     color: var(--black);
-
-    &:hover{
-      text-decoration: underline;
-    }
   }
-
+  a:hover {
+    text-decoration: underline;
+  }
   button {
-    font-family: 'radnika_next',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-
 `;
 
 const InnerStyles = styled.div`
