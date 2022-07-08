4b32891deaafc1090345d96f91a4ae95b40e489a
willyanfx <willyan.fx@gmail.com>
2022-07-07 22:50:49 -0700

    feat: edit form and queries

---
+++ frontend/components/UpdateProduct.js
@@ -0,0 +1,104 @@
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import ErrorMessage from './ErrorMessage';
+import useForm from '../lib/useForm';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  const { data, loading, error } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION);
+  const { inputs, handleChange, resetForm, clearForm } = useForm(data?.Product);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await updateProduct({
+      variables: {
+        id,
+
+        name: inputs.name,
+        description: inputs.description,
+        price: inputs.price,
+      },
+    }).catch(console.error);
+    console.log(res);
+  };
+  if (loading) return <p>Loading ...</p>;
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <ErrorMessage error={error || updateError} />
+      <fieldset aria-busy={loading} disabled={loading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
+}
