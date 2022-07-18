ca1a9853e738b514c8a410dc3bc0f7e132220b15
willyanfx <willyan.fx@gmail.com>
2022-07-17 17:16:42 -0700

    create order and orderItem data type

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -12,6 +12,8 @@ import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { CartItem } from './schemas/CartItem';
 import { extendGraphqlSchema } from './mutations';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fit';
@@ -58,6 +60,8 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      OrderItem,
+      Order,
     }),
     extendGraphqlSchema,
     ui: {
