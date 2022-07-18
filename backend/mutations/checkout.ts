ebacf731be64a2c70cff3b174d8bd53a3f58df1b
willyanfx <willyan.fx@gmail.com>
2022-07-17 19:42:24 -0700

    feat: added stripe to checkout

---
+++ backend/mutations/checkout.ts
@@ -0,0 +1,73 @@
+/* eslint-disable */
+import { KeystoneContext, SessionStore } from '@keystone-next/types';
+
+import {
+  CartItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
+import stripeConfig from '../lib/stripe';
+
+const graphql = String.raw;
+
+async function checkout(
+  root: any,
+  { token }: { token: string },
+  context: KeystoneContext
+): Promise<OrderCreateInput> {
+  const userId = context.session.itemId;
+  if (!userId) {
+    throw new Error('Sorry! You must be signed in to create an order!');
+  }
+
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
+        id
+        name
+        email
+        cart {
+            id
+            quantity
+            product {
+                name
+                price
+                description
+                id
+                photo {
+                    id
+                    image {
+                        id
+                        publicUrlTransformed
+                    }
+                }
+            }
+        }
+    `,
+  });
+
+  console.dir(user, { depth: null });
+
+  let cartItems = user.cart.filter((cartItem) => cartItem.product);
+  const amount = cartItems.reduce(
+    (tally: number, cartItem: CartItemCreateInput) =>
+      tally + cartItem.quantity * cartItem.product.price,
+    0
+  );
+
+  console.log(amount);
+
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: 'USD',
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw new Error(err.message);
+    });
+  console.log(charge);
+}
+
+export default checkout;
