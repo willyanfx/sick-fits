c1294d2dcf624ba9fcdf09cf0b7405c841f5171d
willyanfx <willyan.fx@gmail.com>
2022-07-25 23:36:06 -0700

    test: product

--- frontend/__test__/Product.test.js
+++ frontend/__test__/Product.test.js
@@ -1,28 +1,30 @@
 import { render, screen } from '@testing-library/react';
-import { MockedProvider } from '@apollo/react-testing';
+import { MockedProvider } from '@apollo/client/testing';
 import Product from '../components/Product';
 import { fakeItem } from '../lib/testUtils';
 
 const product = fakeItem();
 
 describe('<Product />', () => {
-  const { container, debug } = render(
-    <MockedProvider mocks={[]}>
-      <Product product={product} />
-    </MockedProvider>
-  );
-  debug();
   it('should render price tag and title', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
     const priceTag = screen.getByText('$50');
     expect(priceTag).toBeInTheDocument();
     const link = container.querySelector('a');
     expect(link).toHaveAttribute('href', '/product/abc123');
     expect(link).toHaveTextContent(product.name);
   });
-  it('should render and matches the snapshot', () => {
-    expect(container).toMatchSnapshot();
-  });
+
   it('should render the image properly', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
     const img = screen.getByText(product.name);
     expect(img).toBeInTheDocument();
   });
