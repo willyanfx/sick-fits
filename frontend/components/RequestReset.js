49ba0e9b00568e48df4825b9c285dafe20b098aa
willyanfx <willyan.fx@gmail.com>
2022-07-27 22:36:32 -0700

    export REQUEST_RESET_MUTATION

--- frontend/components/RequestReset.js
+++ frontend/components/RequestReset.js
@@ -5,7 +5,7 @@ import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
 import ErrorMessage from './ErrorMessage';
 
-const REQUEST_RESET_MUTATION = gql`
+export const REQUEST_RESET_MUTATION = gql`
   mutation REQUEST_RESET_MUTATION($email: String!) {
     sendUserPasswordResetLink(email: $email) {
       message
