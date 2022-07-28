dd7270ff24f30e632349e948bb63a73e9b30bad0
willyanfx <willyan.fx@gmail.com>
2022-07-27 21:36:29 -0700

    export SIGNUP_MUTATION gql

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -5,7 +5,7 @@ import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
 import ErrorMessage from './ErrorMessage';
 
-const SIGNUP_MUTATION = gql`
+export const SIGNUP_MUTATION = gql`
   mutation SIGNUP_MUTATION(
     $name: String!
     $email: String!
